310b375430330b47abbcf920c229e967bbd73e12
JuanDavidPF <JuanFavidPF@gmail.com>
2020-05-11 19:37:50 -0500

    smoothed transitions

--- scripts/fullscreen.js
+++ scripts/fullscreen.js
@@ -23,8 +23,7 @@ function changeMainSize() {
     //full screen size
     main.style.height = window.innerHeight - header.offsetHeight + "px";
     if (smallHeight.matches) main.style.height = "650px";
-    console.log(header.offsetHeight);
-  }, 16);
+  }, 1);
 
   //stops interval from running
   setTimeout(function () {
@@ -32,5 +31,5 @@ function changeMainSize() {
     main.style.height = window.innerHeight - header.offsetHeight + "px";
     if (smallHeight.matches) main.style.height = "650px";
     clearInterval(fixScreen);
-  }, 250);
+  }, 251);
 }
